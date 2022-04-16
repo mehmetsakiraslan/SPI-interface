9584ffec5970c5c33fa3272f5ed51e2b8d94285c
mehmetsakiraslan <87070594+mehmetsakiraslan@users.noreply.github.com>
2022-04-16 20:51:11 +0300

    Update ll_spi.v

--- ll_spi.v
+++ ll_spi.v
@@ -1,37 +1,44 @@
 `timescale 1ns / 1ps
 
-`define PRESCALE        10'd5
-`define FIFO_DEPTH      32'd16
 
-module qspi_interface(
-// AXI4 LITE SLAVE signals
+
+`ifndef GL_RTL_SIM
+`include "sabitler.vh"
+`endif
+
+`define PRESCALE        10'd5
+`define FIFO_DEPTH      `ADRES_BIT
+
+
+module axi_slave_gfi (
+    // AXI4 LITE SLAVE signals
     // Global Signals
     input                       ACLK,
     input                       ARESET,
     // Write Address Channel
-    input [31:0]                AWADDR,
+    input [`ADRES_BIT - 1:0]    AWADDR,
     input                       AWVALID,
     input [2:0]                 AWPROT,
-    output                      AWREADY,    //+
+    output                      AWREADY,
     // Write Data Channel
-    input [31:0]                WDATA,
-    input [3:0]                 WSTRB,
+    input [`VERI_BIT - 1:0]     WDATA,
+    input [`VERI_BIT/8 - 1:0]   WSTRB,
     input                       WVALID,
-    output                      WREADY,     //+
-    // Write Response Channel   // yazma istegi buradan gelcek
+    output                      WREADY,
+    // Write Response Channel
     input                       BREADY,
-    output                      BVALID,     //+
-    output [1:0]                BRESP,      //+
+    output                      BVALID,
+    output [1:0]                BRESP,
     // Read Address Channel
-    input [31:0]                ARADDR,     // adresin en anlamli 8 biti hangi islemin gerceklestirilecegine karar vermek icin kullaniliyor 
+    input [`ADRES_BIT - 1:0]    ARADDR,
     input                       ARVALID,
     input [2:0]                 ARPROT,
-    output                      ARREADY,    //+
-    // Read Data Channel        // okuma isteg, buradan gelcek
+    output                      ARREADY,
+    // Read Data Channel
     input                       RREADY,
-    output [31:0]               RDATA,      //+
-    output                      RVALID,     //+
-    output [1:0]                RRESP,      //+
+    output [`VERI_BIT - 1:0]    RDATA,
+    output                      RVALID,
+    output [1:0]                RRESP,
     // spi i/o
     input               in_so,
     output              out_si,
@@ -50,7 +57,6 @@ module qspi_interface(
     reg [2:0]       state, state_next;
     reg [9:0]       clock_ctr, clock_ctr_next;
     reg [9:0]       bit_ctr, bit_ctr_next;
-    reg             busy, busy_next;
     reg             valid, valid_next; // AXI arayuzu icin
     
     reg [`FIFO_DEPTH-1:0]  fifo, fifo_next;  
@@ -64,7 +70,6 @@ module qspi_interface(
         state_next      = state;
         clock_ctr_next  = clock_ctr;
         bit_ctr_next    = bit_ctr;
-        busy_next       = busy;
         valid_next      = valid;
         fifo_next       = fifo;
         
@@ -78,12 +83,19 @@ module qspi_interface(
             
             IDLE: // IDLE
             begin
-                 if (!busy && (AWADDR[26] || AWADDR[24])) begin // yazma-write istegi, input 
+                 if (AWADDR[24] && WVALID && AWVALID) begin // yazma-write istegi, input 
                     clock_ctr_next  = `PRESCALE - 10'd1;
-                    bit_ctr_next    = AWADDR[28] ? 10'd16 : 10'd8;  //(10'd8 * {AWADDR[29:27]}) - 10'd1; // carpma kullanmadan da yapilabilir.      
+                    bit_ctr_next    = `VERI_BIT;      
                     state_next      = WRITE;
-                    fifo_next       = AWADDR[15:0];
-                    busy_next       = 1'b1;
+                    fifo_next       = WDATA;
+                   
+                    r_cs_next       = 1'b0;
+                    r_sck_next      = 1'b0;
+                 end
+                 else if(ARADDR[25] && ARVALID) begin
+                    clock_ctr_next  = `PRESCALE - 10'd1;
+                    bit_ctr_next    = `VERI_BIT;
+                    state_next      = READ;
                     
                     r_cs_next       = 1'b0;
                     r_sck_next      = 1'b0;
@@ -91,7 +103,6 @@ module qspi_interface(
                  else begin
                     clock_ctr_next  = 10'b0;
                     state_next      = IDLE;
-                    busy_next       = 1'b0;
                     valid_next      = 1'b0;
                     
                     r_cs_next       = 1'b1;
@@ -104,8 +115,8 @@ module qspi_interface(
                 if(bit_ctr == 10'd0) begin
                     state_next      = IDLE;
                     clock_ctr_next  = 10'd0;
-                    valid_next      = 1'b1;
-                    busy_next       = 1'b0;
+                    valid_next      = 1'b1; // valid birden fazla çevrim high'da kaliyor bunu kontrol et.
+                    
                 end
                 else begin
                     r_sck_next  = ~r_sck_next;
@@ -123,11 +134,11 @@ module qspi_interface(
                     state_next      = IDLE;
                     clock_ctr_next  = 10'd0;
                     valid_next      = 1'b1;
-                    busy_next       = 1'b0;
+                    
                 end
                 else begin
                     r_sck_next  = ~r_sck_next;
-                    state_next  = WRITE;
+                    state_next  = READ;
                     if(!r_sck) begin
                         bit_ctr_next    = bit_ctr - 10'd1;
                         fifo_next       = {fifo[`FIFO_DEPTH-2:0], in_so};
@@ -144,7 +155,6 @@ module qspi_interface(
             state   <= 3'b001;
             clock_ctr<= 10'd0;
             bit_ctr <= 10'd0;
-            busy    <= 1'b0;
             valid   <= 1'b0;
             fifo    <= {`FIFO_DEPTH {1'b0}};
             r_cs    <= 1'b1;
@@ -154,7 +164,6 @@ module qspi_interface(
             state   <=  state_next;
             clock_ctr<=  clock_ctr_next;
             bit_ctr <=  bit_ctr_next;
-            busy    <=  busy_next;
             valid   <=  valid_next;
             fifo    <=  fifo_next;
             r_cs    <=  r_cs_next;
